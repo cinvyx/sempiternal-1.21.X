6b4659702b8307321837b44b5bad6efd55138e35
Rohan Patel <rohan@Rohans-MacBook-Pro.local>
2024-12-24 15:31:31 -0800

    The initial commit of this mod. This does not have any functionality whatsoever.

---
+++ src/main/java/com/cinvyx/sempiternal/Sempiternal.java
@@ -0,0 +1,16 @@
+package com.cinvyx.sempiternal;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Sempiternal implements ModInitializer {
+	public static final String MOD_ID = "sempiternal";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/cinvyx/sempiternal/SempiternalClient.java
@@ -0,0 +1,10 @@
+package com.cinvyx.sempiternal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SempiternalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
